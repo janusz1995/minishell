0c9c56b3d363dc3df19d5eab981e49adca7d3b09
Denis <lmidori@mi-h1.msk.21-school.ru>
2020-10-12 17:08:44 +0300

    done2

--- lmidori/path_bin.c
+++ lmidori/path_bin.c
@@ -6,7 +6,7 @@
 /*   By: lmidori <lmidori@student.21-school.ru>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/10/01 21:20:17 by lmidori           #+#    #+#             */
-/*   Updated: 2020/10/05 23:27:56 by lmidori          ###   ########.fr       */
+/*   Updated: 2020/10/12 17:03:52 by lmidori          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char		**path_bin(t_env **head)
 
 	str = NULL;
 	tmp = (*head);
-	while (tmp->next != NULL)
+	while (tmp != NULL)
 	{
 		if ((ft_strncmp(tmp->key_value[0], "PATH=",
 			ft_strlen(tmp->key_value[0]))) == 0)
